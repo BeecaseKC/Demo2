abf361e985040d8c55aa8eadb756c51c8e959d1b
BeecaseKC <beecase_kc@hotmail.com>
2016-05-11 21:10:49 +0100

    Third commit

--- src/Sample.java
+++ src/Sample.java
@@ -3,7 +3,12 @@ public class Sample {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world..!!");
+		runMe();
+		
 
+	}
+	public static void runMe(){
+		System.out.println("runMe method is executing....");
 	}
 
 }
